7745529b5cfab623b82782e2f8c67bd279274349
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-07-15 23:50:14 -0700

    Update rotateArray.java

--- rotateArray.java
+++ rotateArray.java
@@ -6,3 +6,23 @@ For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,
 Note:
 Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 */
+
+//using reverse
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
