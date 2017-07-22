34904c7094047f130f6dd2181f7df0914a6a7238
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-07-21 23:45:58 -0700

    Update FindmaxDistanceInArrays.java

--- FindmaxDistanceInArrays.java
+++ FindmaxDistanceInArrays.java
@@ -1,4 +1,4 @@
-/*
+/* 624. Maximun Distance In Arrays
 Given m arrays, and each array is sorted in ascending order. 
 Now you can pick up two integers from two different arrays (each array picks one) and calculate the distance. 
 We define the distance between two integers a and b to be their absolute difference |a-b|. 
