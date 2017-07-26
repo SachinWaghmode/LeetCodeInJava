1126decf74e430643de4260c4fe90d5c5070c285
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-07-25 23:59:21 -0700

    Update plusOne.java

--- Array/plusOne.java
+++ Array/plusOne.java
@@ -8,3 +8,44 @@ The digits are stored such that the most significant digit is at the head of the
 
 */
 
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int n= digits.length;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int carry [] = new int [n+1];
+        carry[0] = 1;
+        return carry;
+    }
+}
+
+
+/* Traditional Solution
+
+public int[] PlusOne(int[] digits) 
+    {
+        int carry = 1;
+        int index = digits.Length - 1;
+        while (index >= 0 && carry > 0)
+        {
+            digits[index] = (digits[index] + carry) % 10;
+            carry = digits[index] == 0 ? 1 : 0;
+            index--;
+        }
+        
+        if (carry > 0)
+        {
+            digits = new int[digits.Length + 1];
+            digits[0] = 1;
+        }
+        
+        return digits;
+    }
+  */
