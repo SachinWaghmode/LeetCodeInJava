9b39052e7d2f32537bab8701c8d89cf76d98e823
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-07-27 23:51:47 -0700

    Update maximumSubarray.java

--- Array/maximumSubarray.java
+++ Array/maximumSubarray.java
@@ -4,3 +4,16 @@ Find the contiguous subarray within an array (containing at least one number) wh
 For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 */
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int left=nums[0], right=nums[0];
+        
+        for(int i=1; i<nums.length; i++)
+        {
+            right = Math.max(nums[i], right+ nums[i]);
+            left = Math.max(left, right);    
+        }
+        return left;
+    }
+}
