42211a870f7839da606331ec50d64536404ca1cd
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-08-09 02:34:25 -0700

    Update reverseLinkedList.java

--- reverseLinkedList.java
+++ reverseLinkedList.java
@@ -1,2 +1,17 @@
 /* 206. Reverse Linked List
 /*
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null,newHead=null;
+        
+        while(head != null)
+        {
+            prev = newHead;
+            newHead = head;
+            head = head.next;
+            newHead.next = prev; 
+        }
+        return newHead;
+    }
+}
