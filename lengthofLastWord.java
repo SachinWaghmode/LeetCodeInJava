a688f56003f0f078a0fba60776f71f2d31bfdab6
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-08-11 00:13:12 -0700

    Update lengthofLastWord.java

--- lengthofLastWord.java
+++ lengthofLastWord.java
@@ -11,3 +11,22 @@ Given s = "Hello World",
 return 5.
 
 */
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int len=s.length(), wordlength=0;
+        if (len == 0)
+            return 0;
+        while(len>0 && s.charAt(len-1) == ' ')
+        {
+                len--;
+        }
+        
+        while(len > 0 && s.charAt(len-1) != ' ')
+        {
+            wordlength++;
+            len--;
+        }
+        return wordlength;
+    }
+}
