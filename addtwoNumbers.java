9b3ecb666b2542e6cdb18f801d7fad1102024459
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-08-17 00:53:56 -0700

    Update addtwoNumbers.java

--- addtwoNumbers.java
+++ addtwoNumbers.java
@@ -7,3 +7,39 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 
 */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        int newVal = 0;
+        while( l1 != null || l2 != null)
+        {
+            newVal /= 10;
+            if( l1 != null)
+            {
+                newVal += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null)
+            {
+                newVal += l2.val;
+                l2 = l2.next;
+            }
+            
+            temp.next = new ListNode(newVal % 10);
+            temp = temp.next;
+        }
+        if (newVal / 10 == 1)
+            temp.next = new ListNode(1);
+            
+        return head.next;
+    }
+}
