e35345362059c17341afced307bc20525892e5e7
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-09-18 00:45:03 -0700

    Update reverseString.java

--- String/reverseString.java
+++ String/reverseString.java
@@ -4,3 +4,22 @@ Write a function that takes a string as input and returns the string reversed.
 Example:
 Given s = "hello", return "olleh".
 */
+
+class Solution {
+    public String reverseString(String s) {
+        int n = s.length();
+        int i = n-1 , j=0;
+        char[] reversed = s.toCharArray();
+        char temp = ' ';
+        if( s.length() <= 1 || s.isEmpty())
+            return s;
+        while(i>j){
+            temp = reversed[i];
+            reversed[i] = reversed[j];
+            reversed[j] = temp;
+            i--;
+            j++;      
+        }
+        return  String.valueOf(reversed);
+    }
+}
